9031f33621880045c461fd04cb59d89554aef00a
ridhimam1295-ux <ridhimam1295@gmail.com>
2026-04-07 00:59:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function scrollToForm() {
 function openPage(condition) {
   alert("Opening " + condition + " page");
 }
+function openPage(condition) {
+  let message = "Hi Doctor, I need consultation";
+
+  if (condition === "hair") {
+    message = "Hi Doctor, I have Hair Loss problem";
+  } else if (condition === "pcos") {
+    message = "Hi Doctor, I need help for PCOS";
+  } else if (condition === "migraine") {
+    message = "Hi Doctor, I have Migraine issue";
+  }
+
+  window.open(`https://wa.me/91XXXXXXXXXX?text=${encodeURIComponent(message)}`);
+}
