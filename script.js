49c1d10170322c89b42fc12a02e758cc9c8295f7
ridhimam1295-ux <ridhimam1295@gmail.com>
2026-04-07 02:43:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,25 @@ function openPage(condition) {
 
   window.open(url, "_blank");
 }
+let currentIndex = 0;
+const testimonials = document.querySelectorAll(".testimonial");
+
+function showTestimonial(index) {
+  testimonials.forEach((t) => t.classList.remove("active"));
+  testimonials[index].classList.add("active");
+}
+
+function nextTestimonial() {
+  currentIndex = (currentIndex + 1) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+function prevTestimonial() {
+  currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+  showTestimonial(currentIndex);
+}
+
+// AUTO SLIDE
+setInterval(() => {
+  nextTestimonial();
+}, 4000);
