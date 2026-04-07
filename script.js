4576d0b7ae252bc790b9fa54130ea4085e1496c2
ridhimam1295-ux <ridhimam1295@gmail.com>
2026-04-08 01:40:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -100,3 +100,21 @@ Problem: ${problem}`;
 
   window.open(url, "_blank");
 }
+// FAQ TOGGLE
+
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach((q) => {
+  q.addEventListener("click", () => {
+    const answer = q.nextElementSibling;
+
+    // close all others
+    document.querySelectorAll(".faq-answer").forEach((a) => {
+      if (a !== answer) a.style.display = "none";
+    });
+
+    // toggle current
+    answer.style.display =
+      answer.style.display === "block" ? "none" : "block";
+  });
+});
